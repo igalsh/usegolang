001ac42c6d59e3ad19791cb09f33e24b9f101409
igalsh <igal7599@gmail.com>
2017-05-27 13:08:04 +0300

    Added chap3.1 Creating a 404 page

--- chap2_1.go
+++ chap2_1.go
@@ -6,16 +6,19 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-
 	w.Header().Set("Content-Type", "text/html")
-	// Debug
-	// fmt.Fprint(w, r.URL.Path)
 	if r.URL.Path == "/" {
 		fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 	} else if r.URL.Path == "/contact" {
 		fmt.Fprint(w, "To get in touch, please send an email "+
 			"to <a href=\"mailto:support@usegolang.com\">"+
-			"support@usegolang.com</a>.")
+			"support@usegolang.com</a>")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "<h1>We could not find the page you were "+
+			"looking for :(</h1>"+
+			"<p>Please email us if you keep being sent to an "+
+			"invalid page.</p>")
 	}
 }
 func main() {
