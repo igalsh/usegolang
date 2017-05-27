4a7b2f718e9fb83afedfca5168d48f0b5429ae6e
igalsh <igal7599@gmail.com>
2017-05-27 18:17:31 +0300

    Added chap3 Exersize

--- chap3_1.go
+++ chap3_1.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"html"
 	"net/http"
+	"strings"
 
 	"github.com/gorilla/mux"
 )
@@ -17,10 +19,24 @@ func contact(w http.ResponseWriter, r *http.Request) {
 		"to <a href=\"mailto:support@usegolang.com\">"+
 		"support@usegolang.com</a>.")
 }
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>This is FAQ page</h1>")
+}
+func notFound(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>404</h1>")
+	index := strings.Index(r.URL.Path, "/")
+
+	fmt.Fprint(w, html.EscapeString(r.URL.Path[index+1:]))
+}
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
+	r.NotFoundHandler = http.HandlerFunc(notFound)
 	http.ListenAndServe(":3000", r)
 
 }
